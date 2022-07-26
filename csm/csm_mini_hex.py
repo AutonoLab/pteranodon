af573568983fa5ac8cd40e7f47e2315ea577a266
Justin Davis <davis.justin@mssm.org>
2022-07-26 17:17:41 -0600

    Create csm_mini_hex.py

---
+++ csm/csm_mini_hex.py
@@ -0,0 +1,31 @@
+import time
+
+from easy_drone import AbstractDrone
+from RealSense import RealSense
+
+
+# Concrete implemention of DroneInterface using HexSoon edu 450
+class CSM_Mini_Hexsoon(AbstractDrone):
+    def __init__(self, min_follow_dist=5.0, time_slice=0.05) -> None:
+        print("creating camera...")
+        self.cam = RealSense()
+        self.frame = None
+
+        print("running Drone.__init__ ...")
+        super().__init__(address="serial:///dev/ttyACM0",time_slice=time_slice, min_follow_distance=min_follow_dist)
+
+        print("done init")
+
+        self.put(self._drone.param.set_param_int, "COM_ARM_WO_GPS", 1)
+
+    def setup(self):
+        self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
+
+    def loop(self):
+        self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
+
+    def teardown(self):
+        try:
+            self.cam.close()
+        except RuntimeError:
+            pass
