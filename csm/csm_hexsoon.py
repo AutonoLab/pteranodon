95bdabf062c492a2ae1941e0086776872fc1af71
Justin Davis <davis.justin@mssm.org>
2022-07-26 17:17:20 -0600

    Update csm_hexsoon.py

--- csm/csm_hexsoon.py
+++ csm/csm_hexsoon.py
@@ -15,9 +15,11 @@ class CSM_Hexsoon(AbstractDrone):
         self.fp = FrameProcessor(cnn_score_min=0.90, output_path="algo_output.mp4", save_output=True)
 
         print("running Drone.__init__ ...")
-        super().__init__(address="serial:///dev/ttyACM0", time_slice=time_slice, min_follow_distance=min_follow_dist)
+        super().__init__(address="serial:///dev/ttyACM0",time_slice=time_slice, min_follow_distance=min_follow_dist)
 
         print("done init")
+
+        self.put(self._drone.param.set_param_int, "COM_ARM_WO_GPS", 0)
 
     def setup(self):
         self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
@@ -28,16 +30,12 @@ class CSM_Hexsoon(AbstractDrone):
         motion_vector = self.fp.processFrame(self.frame, display=False)
 
         if motion_vector is not None:
+            print("acquired a motion vector")
             x, y = motion_vector
             cam_point = self.cam.deprojectPixelToPoint(depth_frame, cnn_x=x, cnn_y=y)
             # transform the cam_point to the drone_point
             front, right, down = cam_point[2], cam_point[0], 0 - cam_point[1]
-
-            pos = self.telemetry_position_velocity_ned.position
-
-            log_str = f"Dispatching maneuver_to -> front: {front}, right: {right}, down: {down}\n"
-            log_str += f"       Current Position -> north: {pos.north_m}, east: {pos.east_m}, down: {pos.down_m}"
-            self._logger.info(log_str)
+            print(f"DISPATCHING TO: {front}, {right}, {down}")
             self.maneuver_to(front, right, down)
 
     def teardown(self):
