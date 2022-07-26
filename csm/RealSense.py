20a946526b9997aba38297fe3d4464ceaf69c415
Justin Davis <davis.justin@mssm.org>
2022-07-26 16:27:58 -0600

    comment unused (sending it)

--- csm/RealSense.py
+++ csm/RealSense.py
@@ -13,29 +13,31 @@ class RealSense():
         self.pipeline = rs.pipeline()
         self.config = rs.config()
 
-        # Get device product line for setting a supporting resolution
-        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
-        pipeline_profile = self.config.resolve(pipeline_wrapper)
-        device = pipeline_profile.get_device()
-        device_product_line = str(device.get_info(rs.camera_info.product_line))
+#         # Get device product line for setting a supporting resolution
+#         pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+#         pipeline_profile = self.config.resolve(pipeline_wrapper)
+#         device = pipeline_profile.get_device()
+#         device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-        found_rgb = False
-        for s in device.sensors:
-            if s.get_info(rs.camera_info.name) == "RGB Camera":
-                found_rgb = True
-                break
-        if not found_rgb:
-            print("The demo requires Depth camera with Color sensor")
-            exit(0)
+#         found_rgb = False
+#         for s in device.sensors:
+#             if s.get_info(rs.camera_info.name) == "RGB Camera":
+#                 found_rgb = True
+#                 break
+#         if not found_rgb:
+#             print("The demo requires Depth camera with Color sensor")
+#             exit(0)
 
+#         self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+#         if device_product_line == "L500":
+#             self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+#         else:
+#             self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+#         # Start streaming
+#         profile = self.config.resolve(self.pipeline)
         self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-        if device_product_line == "L500":
-            self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-        else:
-            self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        # Start streaming
-        profile = self.config.resolve(self.pipeline)
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
         self.pipeline.start(self.config)
 
     def get_data(self):
