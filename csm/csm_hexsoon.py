afb298c4aa6c09d74cd80312476dd47e3625d854
Justin Davis <davis.justin@mssm.org>
2022-07-27 14:58:10 -0600

    Update csm_hexsoon.py

--- csm/csm_hexsoon.py
+++ csm/csm_hexsoon.py
@@ -7,12 +7,15 @@ from hlca import FrameProcessor
 
 # Concrete implemention of DroneInterface using HexSoon edu 450
 class CSM_Hexsoon(AbstractDrone):
-    def __init__(self, min_follow_dist=5.0, time_slice=0.05) -> None:
+    def __init__(self, min_follow_dist=0.1, time_slice=0.05) -> None:
         print("creating camera...")
         self.cam = RealSense()
         self.frame = None
         print("creating hlca instance...")
         self.fp = FrameProcessor(cnn_score_min=0.90, output_path="algo_output.mp4", save_output=True)
+
+        self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
+        _ = self.fp.processFrame(self.frame, display=False)
 
         print("running Drone.__init__ ...")
         super().__init__(address="serial:///dev/ttyACM0",time_slice=time_slice, min_follow_distance=min_follow_dist)
@@ -22,8 +25,7 @@ class CSM_Hexsoon(AbstractDrone):
         self.put(self._drone.param.set_param_int, "COM_ARM_WO_GPS", 0)
 
     def setup(self):
-        self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
-        _ = self.fp.processFrame(self.frame, display=False)
+        pass
 
     def loop(self):
         self.frame, depth_image, color_frame, depth_frame = self.cam.get_data()
@@ -36,7 +38,7 @@ class CSM_Hexsoon(AbstractDrone):
             # transform the cam_point to the drone_point
             front, right, down = cam_point[2], cam_point[0], 0 - cam_point[1]
             print(f"DISPATCHING TO: {front}, {right}, {down}")
-            self.maneuver_to(front, right, down)
+            self.maneuver_to(front, right, -1 * down, (False, False, True), True)
 
     def teardown(self):
         try:
@@ -54,6 +56,7 @@ if __name__ == "__main__":
 
     key = input("press l to start autonomous flight loop, press any other key to stop flight")
     if key == "l":
+        drone.start_offboard()
         drone.start_loop()
         _ = input("press any key to end autonomous flight")
 
