fb932c922c09469f10a2193b93a54f5e2aaeff7c
justincdavis <davis.justin@mssm.org>
2022-07-29 17:06:04 -0600

    started sensor refactor

--- easy_drone/sensor.py
+++ easy_drone/sensor.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 from .sensor_data import SensorData
 
@@ -6,14 +7,19 @@ from .sensor_data import SensorData
 class Sensor(ABC):
     def __init__(self, name):
         self._name = name
-        self._sensor_data = SensorData(None)
+        self._sensor_data = SensorData()
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
-    def update(self, data):
-        self._sensor_data.update(data)
+    @property
+    def data(self) -> SensorData:
+        return self._sensor_data
+
+    @abstractmethod
+    def update_data(self):
+        pass
 
     @abstractmethod
     def get_data(self):
--- easy_drone/sensor_data.py
+++ easy_drone/sensor_data.py
@@ -1,29 +1,37 @@
 import datetime
+from typing import Any, Optional
 
 
 class SensorData:
-    def __init__(self, data=None):
+    def __init__(self, data: Optional[Any] = None):
         self._timestamp = datetime.datetime.now()
         self._data = data
         self._type = type(data)
 
-    def update(self, data):
+    def __eq__(self, other: "SensorData") -> bool:
+        if isinstance(other, SensorData):
+            return self.data == other.data and self.timestamp == other.timestamp
+        return False
+
+    def __repr__(self) -> str:
+        return f"SensorData({self._timestamp},{self._type},{self._data})"
+
+    def __str__(self) -> str:
+        return f"({self._timestamp},{self._type},{self._data})"
+
+    def update(self, data: Any):
         self._timestamp = datetime.datetime.now()
         self._data = data
         self._type = type(data)
 
     @property
-    def data(self):
+    def data(self) -> Any:
         return self._data
 
-    @data.setter
-    def data(self, data):
-        self._data = data
-
     @property
-    def timestamp(self):
+    def timestamp(self) -> datetime.datetime:
         return self._timestamp
 
     @property
-    def type(self):
+    def type(self) -> Any:
         return self._type
