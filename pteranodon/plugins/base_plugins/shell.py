7d3b182be38f515864fb945c45347dc9226d3f23
Benjamin Carlson <bcarlson336@gmail.com>
2022-09-01 14:46:05 -0600

    Implemented recieve_data method in Shell plugin

--- pteranodon/plugins/base_plugins/shell.py
+++ pteranodon/plugins/base_plugins/shell.py
@@ -1,13 +1,30 @@
 import asyncio
 from asyncio import AbstractEventLoop, Task
 from logging import Logger
+from typing import List
 
 from mavsdk import System, info
 
 from .abstract_base_plugin import AbstractBasePlugin
 
 
+
 class Shell(AbstractBasePlugin):
 
     def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
         super().__init__("shell", system, loop, logger)
+
+        self._feedback_history : List[str] = []
+
+    async def _receive_data(self) -> None:
+        async for data in self._system.shell.receive():
+            self._feedback_history.append(data)
+
+    def get_newest_feedback(self) -> str:
+        return self._feedback_history[-1]
+
+    @property
+    def feedback_history(self) -> List[str]:
+        return self._feedback_history
+
+
