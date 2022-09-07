66a20f35fe2c7116df12a14b914d2288637d3f36
Benjamin Carlson <bcarlson336@gmail.com>
2022-09-07 17:17:37 -0600

    Added get_out_compid implementation

--- pteranodon/plugins/base_plugins/ftp.py
+++ pteranodon/plugins/base_plugins/ftp.py
@@ -3,7 +3,7 @@ from asyncio import AbstractEventLoop
 from logging import Logger
 from typing import List
 
-from mavsdk import System
+from mavsdk import System, ftp
 
 from .abstract_base_plugin import AbstractBasePlugin
 
@@ -13,3 +13,20 @@ class Ftp(AbstractBasePlugin):
 
     def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
         super().__init__("ftp", system, loop, logger)
+        self._comp_id = None
+
+        self._comp_id_task = asyncio.ensure_future(self._get_our_compid(), loop=self._loop)
+
+    async def _get_our_compid(self) -> None:
+        while True:
+            self._id = await self._system.ftp.get_our_compid()
+            break
+
+    def get_our_component_id(self) -> int:
+        """
+        Get our own component ID.
+
+        :return: Our component ID
+        :rtype: uint32
+        """
+        return self._comp_id
