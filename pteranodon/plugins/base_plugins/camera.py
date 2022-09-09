b709a40b8bde1e0bc5c04818042ec034a61855db
Benjamin Carlson <bcarlson336@gmail.com>
2022-09-08 23:25:50 -0600

    Implemented all settings methods.
    Take an extra look at set_setting and get_setting. I gave the option to provide the IDs and the objects instead of just the objects. If this is not what you want because it deviates from the MAV sdk docs, then I will remove it

--- pteranodon/plugins/base_plugins/camera.py
+++ pteranodon/plugins/base_plugins/camera.py
@@ -1,7 +1,9 @@
 import asyncio
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, Task
 from logging import Logger
-from typing import List, Optional
+from typing import List, Optional, Union
+
+from functools import partial
 
 from mavsdk import System, camera
 
@@ -9,14 +11,10 @@ from .abstract_base_plugin import AbstractBasePlugin
 
 # TODO: Methods to implement
 '''
-current_settings
-get_setting
 list_photos
-possible_setting_options
 prepare
 select_camera
 set_mode
-set_setting
 start_photo_interval
 start_video
 start_video_streaming
@@ -32,11 +30,13 @@ class Camera(AbstractBasePlugin):
     def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
         super().__init__("camera", system, loop, logger)
 
-        self._capture_info = None
-        self._information = None
-        self._mode = None
-        self._status = None
-        self._video_stream_info = None
+        self._capture_info : Optional[camera.CaptureInfo] = None
+        self._information : Optional[camera.Information] = None
+        self._mode : Optional[camera.Mode] = None
+        self._status : Optional[camera.Status] = None
+        self._video_stream_info : Optional[camera.VideoStreamInfo] = None
+        self._current_settings : List[camera.Setting] = []
+        self._possible_setting_options: List[camera.SettingOptions] = []
 
         # Tasks of subscribed properties
         self._capture_info_task = asyncio.ensure_future(self._update_capture_info(), loop=self._loop)
@@ -44,6 +44,17 @@ class Camera(AbstractBasePlugin):
         self._mode_task = asyncio.ensure_future(self._update_mode(), loop=self._loop)
         self._status_task = asyncio.ensure_future(self._update_status(), loop=self._loop)
         self._video_stream_info_task = asyncio.ensure_future(self._update_vstream_info(), loop=self._loop)
+
+        # Only want to fetch the current settings and options once on init
+        super().submit_task(
+            asyncio.ensure_future(self._update_current_settings(), loop=self._loop)
+        )
+        super().submit_task(
+            asyncio.ensure_future(self._update_possible_setting_opts(), loop=self._loop)
+        )
+
+        self._system.camera.possible_setting_options()
+
 
     async def _update_capture_info(self) -> None:
         async for info in self._system.camera.capture_info():
@@ -70,6 +81,31 @@ class Camera(AbstractBasePlugin):
             if vstream_info != self._video_stream_info:
                 self._video_stream_info = vstream_info
 
+    async def _update_current_settings(self) -> None:
+
+        # If any of the settings do not have an option set (empty data), do update
+        # If the size is different, then definitely update.
+        should_update_settings = any(setting.option is None for setting in self._current_settings)
+        async for settings in self._system.camera.current_settings():
+            if (len(settings) != len(self._current_settings)) or should_update_settings:
+                self._current_settings = settings
+                should_update_settings = False
+
+    async def _update_possible_setting_opts(self) -> None:
+
+        async for setting_options in self._system.camera.possible_setting_options():
+            if len(setting_options) != len(self._possible_setting_options):
+                self._possible_setting_options = setting_options
+
+                if len(self._current_settings) == 0:
+                    # If current settings have not been fetched yet, fill in setting data with "None" option
+                    #       in the case get_settings is called by the user before they are returned.
+                    # This is overwritten when current_settings returns
+                    self._current_settings = [
+                        camera.Setting(options.setting_id, options.setting_description, None, options.is_range)
+                        for options in self._possible_setting_options
+                    ]
+
     def format_storage(self) -> None:
         """
         Formats the storage (e.g. SD card) in the camera.
@@ -80,11 +116,88 @@ class Camera(AbstractBasePlugin):
             asyncio.ensure_future(self._system.camera.format_storage(), loop=self._loop)
         )
 
+    def set_setting(self, setting : Union[camera.Setting, int], option : Optional[Union[camera.Option, int]] = None) -> None:
+        """
+        Set a setting to some value.
+
+        Examples:
+            `set_setting(some_setting)`
+
+            `set_setting(setting_id, option_id)`
+
+            `set_setting(setting_id, some_option)`
+
+            `set_setting(setting, some_option)`
+
+        :param setting: The setting_id or a Setting object that contains the setting_id.
+        If option is not set, the option_id must be included in this Setting object.
+        :type setting: camera.Setting or uint32
+        :param option: The option_id or an Option object that contains the option_id.
+        Must be set if the Setting object does not in include the option_id.
+        :type option: camera.Option or uint32
+        """
+        setting_obj : camera.Setting = setting
+        if isinstance(setting, int):
+            # Setting ID was passed instead of Setting object
+            setting_obj = camera.Setting(setting, "", None, False)
+
+        if option is not None:
+            option_obj: camera.Option = option
+            if isinstance(option, int):
+                # Option ID was passed instead of Option object
+                option_obj = camera.Option(option, "")
+
+            setting_obj.option = option_obj
+
+        if setting_obj.setting_id is None:
+            self._logger.error("Could not set setting! No setting ID provided in object or function!")
+            return None
+
+        if setting_obj.option is None:
+            self._logger.error("Could not set setting with ID {}! No option provided in object or function!".format(
+                setting_obj.setting_id
+            ))
+            return None
+
+        super().submit_task(
+            asyncio.ensure_future(self._system.camera.set_setting(setting_obj), loop=self._loop)
+        )
+
+        # Update local setting object
+        for setting_idx in range(len(self._current_settings)):
+            if self._current_settings[setting_idx].setting_id == setting_obj.setting_id:
+                self._current_settings[setting_idx].option = setting_obj.option
+                break
+
+    def get_setting(self, setting : Union[camera.Setting, int]) -> Optional[camera.Setting]:
+        """
+        Fetches a setting for the given setting ID (either directly given or set in the Setting object)
+
+        :param setting: The Setting object with the chosen or the setting ID
+        :type setting: camera.Setting or uint32
+        :return: The requested camera.Setting object if found, None otherwise.
+        :rtype: Optional[camera.Setting]
+        """
+        setting_obj : camera.Setting = setting
+        if isinstance(setting, int):
+            setting_obj = camera.Setting(setting, "", None, False)
+
+        if setting_obj.setting_id is None:
+            self._logger.error("Could not get setting! No setting ID provided in object or function!")
+            return None
+
+        for cam_setting in self._current_settings:
+            if cam_setting.setting_id == setting_obj.setting_id:
+                return cam_setting
+
+        return None
+
+
     @property
     def capture_info(self) -> Optional[camera.CaptureInfo]:
         """
         :return: The current capture information
-        :rtype: camera.CaptureInfo
+        :rtype: Optional[camera.CaptureInfo]
         """
         return self._capture_info
 
@@ -92,7 +205,7 @@ class Camera(AbstractBasePlugin):
     def information(self) -> Optional[camera.Information]:
         """
         :return: The current camera information
-        :rtype: camera.Information
+        :rtype: Optional[camera.Information]
         """
         return self._information
 
@@ -100,7 +213,7 @@ class Camera(AbstractBasePlugin):
     def mode(self) -> Optional[camera.Mode]:
         """
         :return: The current camera mode
-        :rtype: camera.Mode
+        :rtype: Optional[camera.Mode]
         """
         return self._mode
 
@@ -108,7 +221,7 @@ class Camera(AbstractBasePlugin):
     def status(self) -> Optional[camera.Status]:
         """
         :return: The current camera status
-        :rtype: camera.Status
+        :rtype: Optional[camera.Status]
         """
         return self._status
 
@@ -116,8 +229,26 @@ class Camera(AbstractBasePlugin):
     def video_stream_info(self) -> Optional[camera.VideoStreamInfo]:
         """
         :return: The current video stream information
-        :rtype: camera.VideoStreamInfo
+        :rtype: Optional[camera.VideoStreamInfo]
         """
         return self._video_stream_info
+
+    @property
+    def possible_settings_options(self) -> List[camera.SettingOptions]:
+        """
+        :return: The list of settings that can be changed
+        :rtype: List[camera.SettingOptions]
+        """
+        return self._possible_setting_options
+
+    @property
+    def current_settings(self) -> List[camera.Setting]:
+        """
+        :return: The list of current camera settings
+        :rtype: List[camera.Setting]
+        """
+        return self._current_settings
+
+
 
 
