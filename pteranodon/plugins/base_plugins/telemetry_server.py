c8a4a3444bb169a6aa06ca2206f98c95a253ab79
manassaini <manas_saini@yahoo.com>
2022-09-13 12:54:14 -0600

    Added imports and finished initialization function

---
+++ pteranodon/plugins/base_plugins/telemetry_server.py
@@ -0,0 +1,17 @@
+import asyncio
+from asyncio import AbstractEventLoop
+from logging import Logger
+
+from mavsdk import System
+from .abstract_base_plugin import AbstractBasePlugin
+
+from mavsdk.telemetry_server import Battery
+
+class TelemetryServer(AbstractBasePlugin):
+    def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
+        super().__init__("telemetry_server", system, loop, logger)
+
+    def publish_battery(self, battery: Battery) -> None:
+        super().submit_task(
+            asyncio.ensure_future(self._system.telemetry_server.publish_battery(battery), loop=)
+        )
