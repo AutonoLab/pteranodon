18772c985a2ba479eb7492fef5896a85439662ed
manassaini <manas_saini@yahoo.com>
2022-09-12 18:18:11 -0600

    Added imports and init function

--- pteranodon/plugins/base_plugins/failure.py
+++ pteranodon/plugins/base_plugins/failure.py
@@ -1,1 +1,17 @@
-#Added a new file
+import asyncio
+from asyncio import AbstractEventLoop, Task
+from logging import Logger
+from time import sleep
+from typing import List, Dict, Any
+
+from mavsdk import System, failure
+#from mavsdk.failure import... not sure if I need something here
+
+from .abstract_base_plugin import AbstractBasePlugin
+
+class Failure(AbstractBasePlugin):
+    def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
+        super().__init__("failure", system, loop, logger)
+
+
+
