395289a57ba8d009d232a29ea7063fe62f38f76a
manassaini <manas_saini@yahoo.com>
2022-09-12 18:27:02 -0600

    Added inject function

--- pteranodon/plugins/base_plugins/failure.py
+++ pteranodon/plugins/base_plugins/failure.py
@@ -5,7 +5,7 @@ from time import sleep
 from typing import List, Dict, Any
 
 from mavsdk import System, failure
-#from mavsdk.failure import... not sure if I need something here
+from mavsdk.failure import FailureUnit, FailureType
 
 from .abstract_base_plugin import AbstractBasePlugin
 
@@ -13,5 +13,9 @@ class Failure(AbstractBasePlugin):
     def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
         super().__init__("failure", system, loop, logger)
 
+    def inject(self, failure_unit : FailureUnit, failure_type : FailureType, instance : int) -> None:
+        super().submit_task(
+            asyncio.ensure_future(self._system.failure.inject(failure_unit, failure_type, instance))
+        )
 
 
