c0bfb110c30b8f7c0162be34165763dae789f4a2
manassaini <manas_saini@yahoo.com>
2022-09-13 20:43:48 -0600

    Completed imports and finished respond_tracking_off_command function

---
+++ pteranodon/plugins/base_plugins/tracking_server.py
@@ -0,0 +1,20 @@
+import asyncio
+from asyncio import AbstractEventLoop
+from logging import Logger
+
+from mavsdk import System
+from .abstract_base_plugin import AbstractBasePlugin
+
+from mavsdk.tracking_server import CommandAnswer, TrackPoint, TrackRectangle
+
+
+class TrackingServer(AbstractBasePlugin):
+    def __init__(self, system: System, loop: AbstractEventLoop, logger: Logger) -> None:
+        super().__init__("tracking_server", system, loop, logger)
+
+    def respond_tracking_off_command(self, command_answer: CommandAnswer) -> None:
+        super().submit_task(
+            asyncio.ensure_future(self._system.tracking_server.respond_tracking_off_command(command_answer),
+                                  loop=self._loop)
+        )
+
