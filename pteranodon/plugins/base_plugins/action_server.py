13f0532112e74c2b3b7625746ade986777b3e3ea
WestonZavadil <westonzavadil@mines.edu>
2022-09-15 12:21:50 -0600

    Added docstrings

--- pteranodon/plugins/base_plugins/action_server.py
+++ pteranodon/plugins/base_plugins/action_server.py
@@ -1,5 +1,5 @@
 import asyncio
-from asyncio import AbstractEventLoop, Task
+from asyncio import AbstractEventLoop
 from logging import Logger
 from typing import Optional
 from threading import Condition
@@ -18,6 +18,7 @@ class ActionSever(AbstractBasePlugin):
         self._arm_disarm_task = asyncio.ensure_future(self._arm_disarm(), loop=self._loop)
         self._arm_disarm_value: Optional[action_server.ArmDisarm] = None
         self._flight_mode_change_task = asyncio.ensure_future(self._flight_mode_change(), loop=self._loop)
+        self._flight_mode_change_value: Optional[action_server.FlightMode] = None
         self._flight_mode_change_value: Optional[action_server.FlightMode] = None
         self._land_task = asyncio.ensure_future(self._land(), loop=self._loop)
         self._land_value: Optional[bool] = None
@@ -55,6 +56,10 @@ class ActionSever(AbstractBasePlugin):
         return self._flight_mode_change_value
 
     def get_allowable_flight_modes(self) -> Optional[action_server.AllowableFlightModes]:
+        """
+        Returns the flight modes allowed
+        :return: action_server.AllowableFlightModes ; all allowed flight modes
+        """
         self._logger.info(f"Pulling allowable flight modes")
 
         get_flight_modes_task = asyncio.ensure_future(
@@ -78,6 +83,10 @@ class ActionSever(AbstractBasePlugin):
                 self._land_value = x
 
     def land(self) -> Optional[bool]:
+        """
+        returns the boolean that explains if the vehicle is landing
+        :return: bool ; True if the vehicle is landing, False otherwise
+        """
         return self._land_value
 
     async def _reboot(self):
@@ -86,27 +95,53 @@ class ActionSever(AbstractBasePlugin):
                 self._reboot_value = x
 
     def reboot(self) -> Optional[bool]:
+        """
+        returnsa the boolean that explains if the vehicle is rebooting
+        :return: bool ; True if the vehicle is rebooting, False otherwise
+        """
         return self._reboot_value
 
     def set_allow_takeoff(self, allow_takeoff: bool):
+        """
+        Sets the boolean that allows the vehicle to takeoff
+        :param allow_takeoff: bool ; True if the vehicle is allowed to take off, False otherwise
+        :return: None
+        """
         self._logger.info(f"setting allow_takeoff to {allow_takeoff}")
         super().submit_task(
             asyncio.ensure_future(self._system.action_server.set_allow_takeoff(allow_takeoff), loop=self._loop)
         )
 
     def set_allowable_flight_modes(self, flight_modes: action_server.AllowableFlightModes):
+        """
+        Sets the flight modes allowable by the vehicle
+        :param flight_modes: action_server.AllowableFlightModes ; a list of allowable flight modes
+        :return: None
+        """
         self._logger.info(f"Setting allowable flight modes to inputted flight modes")
         super().submit_task(
             asyncio.ensure_future(self._system.action_server.set_allowable_flight_modes(flight_modes))
         )
 
     def set_armable(self, armable: bool, force_armable: bool):
+        """
+        Sets if the vehicle is armable or force_armable
+        :param armable: bool ; True if the vehicle is armable, False otherwise
+        :param force_armable: bool ; True if the vehicle is force_armable, False otherwise
+        :return: None
+        """
         self._logger.info(f"setting \"is armable now?\" to {armable}, and \"is armable with force\" to {force_armable}")
         super().submit_task(
             asyncio.ensure_future(self._system.action_server.set_armable(armable, force_armable), loop=self._loop)
         )
 
     def set_disarmable(self, disarmable: bool, force_disarmable: bool):
+        """
+        Sets if the vehicle is disarmable or force_disarmable
+        :param disarmable: bool ; True if the vehicle is disarmable, False otherwise
+        :param force_disarmable: bool ; True if the vehicle is force_disarmable, False otherwise
+        :return: None
+        """
         self._logger.info(
             f"setting \"Is disarmable\" to {disarmable}, and \"is disarmable with force\" to {force_disarmable}"
         )
@@ -122,6 +157,10 @@ class ActionSever(AbstractBasePlugin):
                 self._shutdown_value = x
 
     def shutdown(self) -> Optional[bool]:
+        """
+        returns if the vehicle is in the process of shutting down
+        :return: bool ; True if the vehicle is shutting down, False otherwise
+        """
         return self._shutdown_value
 
     async def _takeoff(self):
@@ -130,6 +169,10 @@ class ActionSever(AbstractBasePlugin):
                 self._takeoff_value = x
 
     def takeoff(self) -> Optional[bool]:
+        """
+        Returns if the vehicle is in the state of taking off
+        :return: bool ; True if the vehicle is taking off, false otherwise
+        """
         return self._takeoff_value
 
     async def _terminate(self):
@@ -138,4 +181,8 @@ class ActionSever(AbstractBasePlugin):
                 self._terminate_value = x
 
     def terminate(self) -> Optional[bool]:
+        """
+        Returns if the mission is being terminated
+        :return: bool ; True if the mission is terminated, False othewise
+        """
         return self._terminate_value
