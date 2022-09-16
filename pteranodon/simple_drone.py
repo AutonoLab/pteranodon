25f083ec3d908f48280acf460794ea924388c9d0
WestonZavadil <westonzavadil@mines.edu>
2022-09-16 14:15:03 -0600

    Added Class docstring for simple_drone

--- pteranodon/simple_drone.py
+++ pteranodon/simple_drone.py
@@ -2,6 +2,9 @@ from .abstract_drone import AbstractDrone
 
 
 class SimpleDrone(AbstractDrone):
+    """
+    A bare minimum "drone" with all methods filled with pass for syntax purposes
+    """
     def __init__(self, address: str):
         super().__init__(address=address)
 
