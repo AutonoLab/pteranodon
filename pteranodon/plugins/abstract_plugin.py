9c2f59ae5bf62f9772c70e8101b9f2c7ea6adff3
justincdavis <jcdavis@mssm.org>
2022-09-30 11:48:39 -0600

    updated AbstractPlugin._submit_coroutine, cleans up memory automatically, saves coro names

--- pteranodon/plugins/abstract_plugin.py
+++ pteranodon/plugins/abstract_plugin.py
@@ -1,11 +1,11 @@
 from abc import ABC
 import asyncio
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, coroutine
 from concurrent.futures import Future
 from logging import Logger
 from collections import deque
 from functools import partial
-import platform
+from typing import Tuple, Any
 
 from mavsdk import System
 
@@ -23,12 +23,9 @@ class AbstractPlugin(ABC):
         self._loop: AbstractEventLoop = loop
         self._logger: Logger = logger
 
-        self._use_coro_names = True
-        if int(platform.python_version().split(".")[1]) < 8:
-            self._use_coro_names = False
-
-        self._task_cache: deque = deque(maxlen=10)
-        self._result_cache: deque = deque(maxlen=10)
+        self._future_cache: deque = deque()
+        self._name_cache: deque = deque()
+        self._result_cache: deque[Tuple[str, Any]] = deque(maxlen=10)
 
     @property
     def name(self) -> str:
@@ -37,26 +34,25 @@ class AbstractPlugin(ABC):
         """
         return self._name
 
-    def _task_callback(self, future: Future) -> None:
-        if self._use_coro_names:
-            # self._logger.info(f"Task completed: {task.get_coro().__qualname__} ")  # type: ignore
-            # TODO, somehow attatch a name
-            pass
+    def _future_callback(self, future: Future) -> None:
+        coro_name: str = self._name_cache[self._future_cache.index(future)]
+        self._logger.info(f"Task completed: {coro_name} ")
         try:
-            self._result_cache.append(future.result())
+            self._result_cache.append((coro_name, future.result()))
         except Exception as e:
             self._logger.error(e)
+        self._future_cache.remove(future)
+        self._name_cache.remove(coro_name)
 
-    # TODO, update the typing on this
-    # TODO, update so it takes the coroutine and runs asyncio.run_coroutine_threadsafe for you
-    def _submit_coroutine(self, new_funcall) -> Future:
+    def _submit_coroutine(self, coro: coroutine) -> Future:
         """
-        Puts a task returned by asyncio.run_coroutine_threadsafe to the task_cache to prevent garbage collection and allow return
+        Puts a task returned by asyncio.run_coroutine_threadsafe to the future_cache to prevent garbage collection and allow return
         value analysis
         :param new_task: A concurrent.future.Future
         :return: The submitted task, if plugin specific callbacks are added
         """
-        new_future = asyncio.run_coroutine_threadsafe(new_funcall, loop=self._loop)
-        new_future.add_done_callback(partial(self._task_callback))
-        self._task_cache.append(new_future)
+        new_future = asyncio.run_coroutine_threadsafe(coro, loop=self._loop)
+        new_future.add_done_callback(partial(self._future_callback))
+        self._future_cache.append(new_future)
+        self._name_cache.append(coro.__qualname__)  # type: ignore
         return new_future
