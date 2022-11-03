f856804d2d6f3825a48a71c5c6320d035643aa03
manassaini <manas_saini@yahoo.com>
2022-11-03 14:38:43 -0600

    Finished examples for manual_control.py

---
+++ examples/manual_control.py
@@ -0,0 +1,16 @@
+from pteranodon import SimpleDrone
+
+def run():
+    drone = SimpleDrone("udp://:14540")
+
+    print("-- Arming")
+    drone.action.arm()
+
+    print("-- Taking off")
+    drone.action.takeoff()
+
+    print("-- Starting manual control")
+    drone.manual_control.start_position_control()
+
+if __name__ == "__main__":
+    run()
