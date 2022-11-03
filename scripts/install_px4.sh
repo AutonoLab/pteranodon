6b142ed9f92a6c2c933181aee3c4695879748b01
David Shearon <davidisawesome369@gmail.com>
2022-11-02 21:31:23 -0600

    Added newline at end

--- scripts/install_px4.sh
+++ scripts/install_px4.sh
@@ -14,16 +14,6 @@ else
 	git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 fi
 
-
-echo "Installing Boost................................................................"
-# sudo apt-get install libboost-all-dev
-
-echo "Installing Gazebo..............................................................."
-# sudo apt-get install gazebo
-
-# sudo apt update
-# sudo apt upgrade
-
 echo "Running ubuntu.sh..............................................................."
 sudo bash ./PX4-Autopilot/Tools/setup/ubuntu.sh --sim_jammy
 
