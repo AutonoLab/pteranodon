cbedddbbaaa2f0664ea7c3bf8503c59b3ab57a8b
David Shearon <davidisawesome369@gmail.com>
2022-11-02 19:17:56 -0600

    Changed shebang

--- scripts/install_px4.sh
+++ scripts/install_px4.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #Download and install PX4.
 # Run with: sudo bash ./install_px4.sh
@@ -14,11 +14,18 @@ else
 	git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 fi
 
+echo "Installing Boost................................................................"
+sudo apt-get install libboost-all-dev
+
+echo "Installing Gazebo..............................................................."
+sudo apt-get install gazebo
+
 echo "Running ubuntu.sh..............................................................."
 sudo bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
 
 echo "Installing pip dependencies....................................................."
 pip install -r ./pteranodon/requirements.txt
+
 
 echo "Running Gazebo.................................................................."
 cd ./PX4-Autopilot
