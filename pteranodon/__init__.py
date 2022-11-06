36bd6c26f32a074ef2fed0bc8e4ea1f1f77e0de7
Benjamin Carlson <bcarlson336@gmail.com>
2022-11-05 19:39:26 -0600

    Fixed pylint

--- pteranodon/__init__.py
+++ pteranodon/__init__.py
@@ -1,6 +1,5 @@
 from .abstract_drone import AbstractDrone
 from .simple_drone import SimpleDrone
 from .plugins import AbstractCustomPlugin
-from pteranodon.utils.server_detector import ServerDetector
 
-__all__ = ["AbstractDrone", "SimpleDrone", "AbstractCustomPlugin", "ServerDetector"]
+__all__ = ["AbstractDrone", "SimpleDrone", "AbstractCustomPlugin"]
