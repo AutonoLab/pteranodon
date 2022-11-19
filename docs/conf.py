0e1647441926b7a9505394ef3cb0fd6b8d124e6a
Justin Davis <davis.justin@mssm.org>
2022-11-19 15:06:45 -0700

    removed merge markers

--- docs/conf.py
+++ docs/conf.py
@@ -8,19 +8,6 @@
 
 import os
 import sys
-<<<<<<< HEAD
-#import pteranodon
-
-sys.path.insert(0, os.path.abspath('..'))
-
-
-
-project = 'Pteranodon'
-copyright = '2022, Field Session'
-author = 'Field Session'
-=======
->>>>>>> 197ea54696175e840e0e4b10dd9d16ae938abc4a
-
 # import pteranodon
 
 sys.path.insert(0, os.path.abspath(".."))
@@ -33,31 +20,13 @@ author = "Field Session"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-<<<<<<< HEAD
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode']
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-=======
->>>>>>> 197ea54696175e840e0e4b10dd9d16ae938abc4a
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-<<<<<<< HEAD
-=======
-
->>>>>>> 197ea54696175e840e0e4b10dd9d16ae938abc4a
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-<<<<<<< HEAD
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-=======
->>>>>>> 197ea54696175e840e0e4b10dd9d16ae938abc4a
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
