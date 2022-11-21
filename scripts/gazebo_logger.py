e319202779f27f3188d7af42d83374e209935192
David Shearon <davidisawesome369@gmail.com>
2022-11-21 16:35:56 -0700

    Fixed small error

--- scripts/gazebo_logger.py
+++ scripts/gazebo_logger.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from pteranodon import SimpleDrone
 
@@ -9,10 +10,10 @@ drone = SimpleDrone("udp://:14540")
 print("Waitiing for the drone to connect...")
 
 
-if drone.core.connection_state().is_connected:
-    print("Drone CONNECTED")
-
-
+while (not drone.core.connection_state().is_connected):
+    time.sleep(2)
+        
+print("Drone CONNECTED")
 
 entries = drone.log_files.get_entries()
 
