5d215548b86ac46b90c761044ce5ffedf624b2c5
David Shearon <davidisawesome369@gmail.com>
2022-11-21 23:49:37 -0700

    Removed try except

--- scripts/gazebo_logger.py
+++ scripts/gazebo_logger.py
@@ -32,8 +32,6 @@ if(len(entries) != 0):
 for count in range(len(entries)):
     filename = "log_" + str(count)
     
-    try:
-        drone.log_files.download_log_file(entries[count], str(DIR + "RAW/" + filename + ".txt"), 15.0)
-        os.system("ulog2csv -o " + DIR + "CSVs/" + filename + " " + DIR + "RAW/" + filename + ".txt")
-    except:
-        print("File not converted")
+    drone.log_files.download_log_file(entries[count], str(DIR + "RAW/" + filename + ".txt"), 15.0)
+    os.system("ulog2csv -o " + DIR + "CSVs/" + filename + ".csv " + DIR + "RAW/" + filename + ".txt")
+
