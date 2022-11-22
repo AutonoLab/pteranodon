c9629921605d3474164d8c361b600ce097cbe6c9
David Shearon <davidisawesome369@gmail.com>
2022-11-21 22:12:28 -0700

    changed for loop to indexed

--- scripts/gazebo_logger.py
+++ scripts/gazebo_logger.py
@@ -3,26 +3,16 @@ import time
 
 from pteranodon import SimpleDrone
 
+DIR = "../gazebo_logfiles/raw/"
+
 print("Initiating drone...")
 
 drone = SimpleDrone("udp://:14540")
-
 entries = drone.log_files.get_entries()
-
-
-DIR = "../gazebo_logfiles/raw/"
 
 if(len(entries) != 0):
     if not os.path.exists(DIR):
         os.makedirs(DIR)
 
-count = 0
-
-for entry in entries:
-    drone.log_files.download_log_file(entry, str(DIR + str(count) + ".txt"), 5.0)
-    count = count + 1
-
-
-    
-
-
+for count in range(len(entries)):
+    drone.log_files.download_log_file(entries[count], str(DIR + str(count) + ".txt"), 5.0)
