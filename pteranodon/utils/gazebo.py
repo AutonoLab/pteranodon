67f86fc58e66e79481937635fb5a91f806a42f3b
justincdavis <davis.justin@mssm.org>
2022-11-22 13:24:33 -0700

    fixed formatting

--- pteranodon/utils/gazebo.py
+++ pteranodon/utils/gazebo.py
@@ -3,11 +3,13 @@ import subprocess
 
 def kill_gz_server() -> None:
     """Kill all gzserver processes."""
-    subprocess.run(["killall", "-9", "gzserver"])
+    subprocess.run(["killall", "-9", "gzserver"], check=True)
+
 
 def kill_gz_client() -> None:
     """Kill all gzclient processes."""
-    subprocess.run(["killall", "-9", "gzclient"])
+    subprocess.run(["killall", "-9", "gzclient"], check=True)
+
 
 def kill_gazebo() -> None:
     """Kill all gzserver and gzclient processes."""
