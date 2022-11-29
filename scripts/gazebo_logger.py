d5420323e885d12d534d61f57e6c0fc78c58179a
David Shearon <davidisawesome369@gmail.com>
2022-11-28 22:52:56 -0700

    Removed extra file. Added try except

--- scripts/gazebo_logger.py
+++ scripts/gazebo_logger.py
@@ -32,6 +32,14 @@ if(len(entries) != 0):
 for count in range(len(entries)):
     filename = "log_" + str(count)
     
-    drone.log_files.download_log_file(entries[count], str(DIR + "RAW/" + filename + ".txt"))
+    while True:
+        try:
+            drone.log_files.download_log_file(entries[count], str(DIR + "RAW/" + filename + ".txt"))
+            time.sleep(10)
+            break
+        except:
+            time.sleep(1)
+
+    
     os.system("ulog2csv -o " + DIR + "CSVs/" + filename + ".csv " + DIR + "RAW/" + filename + ".txt")
 
