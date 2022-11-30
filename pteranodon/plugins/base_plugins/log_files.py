6ae56ca2fe64398d4ffdd84e2e3464d7ba632786
David Shearon <davidisawesome369@gmail.com>
2022-11-29 20:33:36 -0700

    Increased timeout

--- pteranodon/plugins/base_plugins/log_files.py
+++ pteranodon/plugins/base_plugins/log_files.py
@@ -27,7 +27,7 @@ class LogFiles(AbstractBasePlugin):
 
         self._end_init()
 
-    def download_log_file(self, entry: Entry, path: str, timeout: float = 100.0) -> Optional[ProgressData]:
+    def download_log_file(self, entry: Entry, path: str, timeout: float = 200.0) -> Optional[ProgressData]:
         """
         Download log file synchronously.
 
