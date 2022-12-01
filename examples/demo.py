7ea09e49aa8f728c8b0dc65967e7ed7e0a10e691
David Shearon <davidisawesome369@gmail.com>
2022-12-01 09:01:25 -0700

    debugging

--- examples/demo.py
+++ examples/demo.py
@@ -5,67 +5,66 @@ from mavsdk.gimbal import ControlMode, GimbalMode
 input("\ndrone = SimpleDrone('udp://:14540')\n")
 drone = SimpleDrone("udp://:14540")
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.arm()\n")
 drone.arm()
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.takeoff()\n")
 drone.takeoff()
 
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.gimbal.take_control(ControlMode.PRIMARY)\n")
 drone.gimbal.take_control(ControlMode.PRIMARY)
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.gimbal.set_mode(GimbalMode.YAW_FOLLOW)\n")
 drone.gimbal.set_mode(GimbalMode.YAW_FOLLOW)
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.put(drone.gimbal.set_pitch_and_yaw, -90, 0)\n")
 drone.put(drone.gimbal.set_pitch_and_yaw, -90, 0)
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)\n")
 drone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)
 
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.geofence.clear_geofence()\n")
 drone.geofence.clear_geofence()
 
-time.sleep(2)
 position = drone.telemetry.home
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.put(drone.action.goto_location, position.latitude_deg, position.longitude_deg, 4, 0)\n")
 drone.put(drone.action.goto_location, position.latitude_deg + 0.0001, position.longitude_deg, 4, 0)
 
-time.sleep(2)
-input("\ndrone.put(drone.action.goto_location, position['latitude_deg'], position['longitude_deg'], 4, 0)\n")
+time.sleep(1)
+input("\ndrone.put(drone.action.goto_location, position.latitude_deg, position.longitude_deg + 0.0001, 4, 0)\n")
 drone.put(drone.action.goto_location, position.latitude_deg, position.longitude_deg + 0.0001, 4, 0)
 
-time.sleep(2)
-input("\ndrone.put(drone.action.goto_location, position['latitude_deg'], position['longitude_deg'], 4, 0)\n")
+time.sleep(1)
+input("\ndrone.put(drone.action.goto_location, position.latitude_deg + 0.0001, position.longitude_deg + 0.0001, 4, 0)\n")
 drone.put(drone.action.goto_location, position.latitude_deg + 0.0001, position.longitude_deg + 0.0001, 4, 0)
 
-time.sleep(2)
-input("\ndrone.put(drone.action.goto_location, position['latitude_deg'], position['longitude_deg'], 4, 0)\n")
+time.sleep(1)
+input("\ndrone.put(drone.action.goto_location, position.latitude_deg, position.longitude_deg, 4, 0)\n")
 drone.put(drone.action.goto_location, position.latitude_deg, position.longitude_deg, 4, 0)
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.land()\n")
 drone.land()
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.disarm()\n")
 drone.disarm()
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.wait_until_queue_empty()\n")
 drone.wait_until_queue_empty()
 
-time.sleep(2)
+time.sleep(1)
 input("\ndrone.stop()\n")
 drone.stop()
