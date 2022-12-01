06a97fe3ba79ddfbb01adfcb2cc505f181089f49
David Shearon <davidisawesome369@gmail.com>
2022-12-01 07:43:24 -0700

    Added gimbal commands

--- examples/demo.py
+++ examples/demo.py
@@ -1,5 +1,6 @@
 from pteranodon import SimpleDrone
 import time
+from mavsdk.gimbal import ControlMode, GimbalMode
 
 input("\ndrone = SimpleDrone('udp://:14540')\n")
 drone = SimpleDrone("udp://:14540")
@@ -11,6 +12,28 @@ drone.arm()
 time.sleep(2)
 input("\ndrone.takeoff()\n")
 drone.takeoff()
+
+
+time.sleep(2)
+input("\ndrone.gimbal.take_control(ControlMode.PRIMARY)\n")
+drone.gimbal.take_control(ControlMode.PRIMARY)
+
+time.sleep(2)
+input("\ndrone.gimbal.set_mode(GimbalMode.YAW_FOLLOW)\n")
+drone.gimbal.set_mode(GimbalMode.YAW_FOLLOW)
+
+time.sleep(2)
+input("\ndrone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)\n")
+drone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)
+
+time.sleep(2)
+input("\ndrone.put(drone.gimbal.set_pitch_and_yaw, -90, 0)\n")
+drone.put(drone.gimbal.set_pitch_and_yaw, -90, 0)
+
+time.sleep(2)
+input("\ndrone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)\n")
+drone.put(drone.gimbal.set_pitch_and_yaw, 0, 0)
+
 
 time.sleep(2)
 input("\ndrone.geofence.clear_geofence()\n")
