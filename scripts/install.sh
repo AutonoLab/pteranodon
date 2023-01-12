4ed7db64ffe1289e86428d9aa217bc1aa67db0b1
justincdavis <davis.justin@mssm.org>
2023-01-12 12:57:17 -0700

    testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,3 @@
 sudo apt-get install -y git-all
 sudo apt install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
 sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0
-sudo apt-get install -y python-gobject
