42468a1c4f41271957c3563d1add111cee9996e0
justincdavis <davis.justin@mssm.org>
2023-01-12 12:55:32 -0700

    added another apt-get

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,4 @@
 # handles command line installation of tools which pteranodon users will use or are likely to use
 sudo apt-get install -y git-all
 sudo apt install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+sudo apt-get install -y python-gobject
