4fd9eef0f8deec8e7b7889d033519b3a67571d2a
justincdavis <davis.justin@mssm.org>
2023-02-10 12:31:50 -0700

    first

---
+++ pteranodon/implementations/hardware/virtual_realsense.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from ...plugins.extension_plugins.sensor import AbstractSensor
+
+
+class VirtualRealSense(AbstractSensor):
+    """Class implementation for a virtual RealSense camera"""
+
+    def __init__(self, width=640, height=480, fps=30):
+        pass
