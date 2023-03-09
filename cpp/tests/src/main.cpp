a5a3838d74d9589fc65f7699d246e9b08c884487
justincdavis <davis.justin@mssm.org>
2023-03-09 14:14:33 -0700

    Added benchmark to tests for pteranodon_ext

--- cpp/tests/src/main.cpp
+++ cpp/tests/src/main.cpp
@@ -1,26 +1,12 @@
-#include <pteranodon_ext>
+#include "test_detectors.hpp"
+#include "benchmark_detectors.hpp"
 
-#include <vector>
-#include <iostream>
-
-#include <opencv2/opencv.hpp>
-
-int main() {
-    cv::Mat image = cv::imread("data/test.png");
-    cv::Rect anchor = cv::Rect(0, 0, 100, 100);
-    BlobDetector detector(true, false);
+int main(int argc, char** argv)
+{
+    if(testDetectors() != 0)
+        return -1;
+    if(benchmarkDetectors() != 0)
+        return -1;
     
-    // detect with an anchor box
-    cv::Rect blob = detector.detectAnchor(image, anchor);
-    std::cout << "Results from detect with anchor: " << std::endl;
-    std::cout << blob << std::endl;
-
-    // detect without an anchor box
-    std::vector<cv::Rect> blobs = detector.detect(image);
-    std::cout << "Results from detect without anchor: " << std::endl;
-    for (auto blob : blobs) {
-        std::cout << blob << std::endl;
-    }
-
     return 0;
 }
