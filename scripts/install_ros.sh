4d75b627da1a9214f7b35ba07d6d85c808a878fa
AustinHCopley <austinhcopley@gmail.com>
2023-07-12 16:14:23 -0500

    fix docker, change ros install type

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -5,5 +5,5 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade
-sudo apt install ros-foxy-ros-base python3-argcomplete
+sudo apt install ros-foxy-desktop python3-argcomplete
 source /opt/ros/foxy/setup.bash
